574e68a884b02b182e7e89bd065a9e3e0335dc94
Pedro  Teixeira <pedroteixeir@me.com>
2020-11-17 18:39:52 -0300

    Finished animals.py

--- animals.py
+++ animals.py
@@ -3,13 +3,16 @@ class Animal:
     def __init__(self,a,b):
         self.name=a
         self.species=b
+    def __repr__(self):
+        return f"This animal is a {self.species}, which is named {self.name}"
 
 class Cat(Animal):
     def __init__(self,name,spe,owner):
         self.owner = owner
-        Animal.__init__(self,a,b)
+        Animal.__init__(self,name,spe)
 
 a = Animal("Rex","Dog")
 b = Cat("Tom","Persian","James")
-print(a.name, a.species)
-print(b.name,b.species,b.owner)
+
+print(a)
+print(b)
