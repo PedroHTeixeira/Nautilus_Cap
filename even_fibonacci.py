b655e5d07ef14b60878348dc59e3ca0bee7f0a25
Pedro  Teixeira <pedroteixeir@me.com>
2020-11-17 12:33:30 -0300

    Changed sum for s

--- even_fibonacci.py
+++ even_fibonacci.py
@@ -1,12 +1,14 @@
 i=2
 l = [1,2]
-sum = 0
+s = 0
 j=0
-while i<4*10**6:
+while i<5:
     l.append(l[i-2]+ l[i-1])
     i=i+1
 
+
+
 for j in l:
     if j % 2 != 0 :
-        sum = sum +j
-print(sum)
+        s = s +j
+print(s)
--- mult.py
+++ mult.py
@@ -1,13 +1,13 @@
 i=0
 j=0
 l = []
-sum = 0
+s = 0
 while i < 1000:
     l.append(i)
     i = i+1
 
 for j in l:
     if j%3 == 0 or j%5==0:
-        sum += j
+        s += j
 
-print(sum)
+print(s)
