eb89652951746b7ec75570a23d84cde8cc328575
Pedro  Teixeira <pedroteixeir@me.com>
2020-11-17 12:52:30 -0300

    Finished Version Even_Fibonacci

--- even_fibonacci.py
+++ even_fibonacci.py
@@ -2,13 +2,13 @@ i=2
 l = [1,2]
 s = 0
 j=0
-while i<5:
+while l[-1] <4*10**6:
     l.append(l[i-2]+ l[i-1])
     i=i+1
 
 
 
 for j in l:
-    if j % 2 != 0 :
+    if j % 2 == 0 :
         s = s +j
 print(s)
