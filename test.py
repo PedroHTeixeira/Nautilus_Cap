08f69487449cdc1427a6dc79dc47e3dd3940da36
Pedro  Teixeira <pedroteixeir@me.com>
2020-11-17 11:19:25 -0300

    Updated test.py

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-a=input("Diga seu nome")
-print("voce se chama",a)
+i=0
+j=0
+l = []
+sum = 0
+while i < 1000:
+    l.append(i)
+    i = i+1
+
+for j in l:
+    if j%3 == 0 or j%5==0:
+        sum += j
+
+print(sum)
