6fedf217ccfc2538fb92c501b5eec16686d7beaa
Pedro  Teixeira <pedroteixeir@me.com>
2020-11-17 18:33:23 -0300

    Added comments to the files

--- animals.py
+++ animals.py
@@ -1,3 +1,4 @@
+# PYTHON OO 17/11/2020 17:00 Part-1
 class Animal:
     def __init__(self,a,b):
         self.name=a
@@ -9,6 +10,6 @@ class Cat(Animal):
         Animal.__init__(self,a,b)
 
 a = Animal("Rex","Dog")
-b = Cat("Tom","Persian","Josis")
+b = Cat("Tom","Persian","James")
 print(a.name, a.species)
 print(b.name,b.species,b.owner)
