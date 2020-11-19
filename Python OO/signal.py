4f6ca2e728421b294d7f9fb6dd8d3e78da347b52
Pedro  Teixeira <pedroteixeir@me.com>
2020-11-19 03:14:59 -0300

    Finished signal.py

--- Python OO/signal.py
+++ Python OO/signal.py
@@ -5,21 +5,24 @@ class Signal():
     
     def __init__(self,label):
         self.label = label
-        self.amp = numpy.random.normal(-1,1,size="50")
+        self.waves = numpy.random.normal(-1,1,size=50)
         self.tx=5
+        self.amp= sum(list(self.waves))/len(list(self.waves))
 
     def __repr__(self):
-        return "The signal {} has {} of amplitude and {} of recording tax ".format(self.label,self.amp,self.tx)
+        return "The signal {} has {} of amplitude and {} of recording tax. The signal is {}{}".format(self.label,self.amp,self.tx,self.waves,"\n")
 
     def __sub__(self,other):
         if type(self)==type(other):
-            self.label= self.label + "-" + other.label
-            return self.amp - other.amp
+            w = Signal(self.label + "-" + other.label)
+            w.waves=self.waves - other.waves
+            return w
 
     def __add__(self,other):
         if type(self)==type(other):
-            self.label= self.label + "+" + other.label
-            return self.amp + other.amp
+            w = Signal(self.label + "+" + other.label)
+            w.waves=self.waves + other.waves
+            return w
 
     def __gt__(self,other):
         if type(self)==type(other):
@@ -27,8 +30,9 @@ class Signal():
 
     def __mul__(self, other):
         if type(self)==type(other):
-            self.label= self.label + "*" + other.label
-            return self.amp + other.amp
+            w = Signal(self.label + "*" + other.label)
+            w.waves = numpy.array(list(self.waves) + list(other.waves))
+            return w
 
     def __lt__(self,other):
         if type(self)==type(other):
@@ -42,7 +46,23 @@ class Signal():
         if type(self)==type(other):
             return self.amp != other.amp
 
+    def __getitem__(self, index):
+        a=list(self.waves)
+        self.waves = a[index]
+        return self
+
 
 signal1=Signal("Track 1")
-
+signal2=Signal("Track 2")
+signal3=signal1+signal2
+signal4=signal1-signal2
+signal5=signal1*signal2
+signals = [signal1,signal2,signal3,signal4,signal5]
+signals.sort()
 print(signal1)
+print(signal2)
+print(signal3)
+print(signal4)
+print(signal5)
+print(signals)
+print('This is slicing: {}'.format(signal1[3:9]))
